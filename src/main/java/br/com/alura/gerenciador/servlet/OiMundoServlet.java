f74ae9adcbd8596cc23554f12ed39f09ac57bffd
unknown <creusodagama3@hotmail.com>
2023-02-14 09:48:02 -0300

    msg

--- src/main/java/br/com/alura/gerenciador/servlet/OiMundoServlet.java
+++ src/main/java/br/com/alura/gerenciador/servlet/OiMundoServlet.java
@@ -11,6 +11,8 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(urlPatterns = "/oi")
 public class OiMundoServlet extends HttpServlet{
+	
+	private static final long serialVersionUID = 1L;
 
 	//o override serve para sobrescrever o método da classe mãe
 	@Override
